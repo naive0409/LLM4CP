5d06a59d44afe570ad2bf28c517cf728ffe2a1a1
wu <2329140207@qq.com>
2025-01-12 22:42:14 +0800

    test fdd snr

--- test_fdd_full.py
+++ test_fdd_full.py
@@ -17,16 +17,18 @@ import tqdm
 from pvec import pronyvec
 from PAD import PAD3
 from scipy.io import savemat
+import os
 
 if __name__ == "__main__":
     # demo
-    device = torch.device('cuda:1')
+    device = torch.device('cuda:0')
     is_U2D = 1
     prev_path = "./Testing Dataset/H_U_his_test.mat"
     pred_path = "./Testing Dataset/H_U_pre_test.mat"
     pred_path_fdd = "./Testing Dataset/H_D_pre_test.mat"
+    date_ = '20250107_21_59'
     model_path = {
-        'clip': 'Weights/full_shot_fdd/20241116_17_42/clip.copy.pth',
+        'clip': 'Weights/full_shot_fdd/{}/clip.pth'.format(date_),
         'gpt': './Weights/full_shot_fdd/U2D_LLM4CP.pth',
         'transformer': './Weights/full_shot_fdd/U2D_trans.pth',
         'cnn': './Weights/full_shot_fdd/U2D_cnn.pth',
@@ -54,59 +56,67 @@ if __name__ == "__main__":
         print("loading ", i + 1, "th model......", model_test_enable[i])
         if model_test_enable[i] not in ['pad', 'pvec', 'np']:
             model = torch.load(model_path[model_test_enable[i]], map_location=device).to(device)
-        for speed in range(0, 10):
-            test_loss_stack = []
-            test_loss_stack_se = []
-            test_loss_stack_se0 = []
-            test_data_prev = test_data_prev_base[[speed], ...]
-            test_data_pred = test_data_pred_base[[speed], ...]
-            test_data_prev = rearrange(test_data_prev, 'v b l k n m c -> (v b c) (n m) l (k)')
-            test_data_pred = rearrange(test_data_pred, 'v b l k n m c -> (v b c) (n m) l (k)')
-            std = np.sqrt(np.std(np.abs(test_data_prev) ** 2))
-            test_data_prev = test_data_prev / std
-            test_data_pred = test_data_pred / std
-            lens, _, _, _ = test_data_prev.shape
-            if model_test_enable[i] in ['clip', 'gpt', 'transformer', 'rnn', 'lstm', 'gru', 'cnn', 'np']:
-                if model_test_enable[i] != 'np':
-                    model.eval()
-                prev_data = LoadBatch_ofdm_2(test_data_prev)
-                pred_data = LoadBatch_ofdm_2(test_data_pred)
-                bs = 64
-                cycle_times = lens // bs
-                filename = 'code_testing/csi_output/20241116_17_42/' + '{}.mat'.format((speed+1)*10)
-                ground_truth = []
-                model_outputs = []
-                with torch.no_grad():
-                    for cyt in range(cycle_times):
-                        prev = prev_data[cyt * bs:(cyt + 1) * bs, :, :].to(device)
-                        pred = pred_data[cyt * bs:(cyt + 1) * bs, :, :].to(device)
-                        prev = rearrange(prev, 'b m l k -> (b m) l k')
-                        pred = rearrange(pred, 'b m l k -> (b m) l k')
-                        if model_test_enable[i] == 'gpt':
-                            out = model(prev, None, None, None)
-                        elif model_test_enable[i] == 'transformer':
-                            encoder_input = prev
-                            dec_inp = torch.zeros_like(encoder_input[:, -pred_len:, :]).to(device)
-                            decoder_input = torch.cat([encoder_input[:, prev_len - label_len:prev_len, :], dec_inp],
-                                                      dim=1)
-                            out = model(encoder_input, decoder_input)
-                        elif model_test_enable[i] in ['lstm', 'rnn', 'gru']:
-                            out = model(prev, pred_len, device)
-                        elif model_test_enable[i] == 'cnn':
-                            out = model(prev)
-                        elif model_test_enable[i] == 'np':
-                            out = prev[:, [-1], :].repeat([1, pred_len, 1])
-                        elif model_test_enable[i] == 'clip':
-                            clip_loss, out = model(prev, None, None, None)
-                        loss = criterion(out, pred)
-                        test_loss_stack.append(loss.item())
-                        ground_truth.append(pred.cpu().detach().numpy())
-                        model_outputs.append(out.cpu().detach().numpy())
-                savemat(filename, {'ground_truth':np.array(ground_truth),'model_output':np.array(model_outputs)})
-                print("speed", (speed + 1) * 10, ":  NMSE:", np.nanmean(np.array(test_loss_stack)))
-                NMSE[i].append(np.nanmean(np.array(test_loss_stack)))
+        for snr in [0,5,10,15,20,25]:
+            for speed in range(0, 1):
+                test_loss_stack = []
+                test_loss_stack_se = []
+                test_loss_stack_se0 = []
+                test_data_prev = test_data_prev_base[[speed], ...]
+                test_data_pred = test_data_pred_base[[speed], ...]
+                test_data_prev = rearrange(test_data_prev, 'v b l k n m c -> (v b c) (n m) l (k)')
+                test_data_pred = rearrange(test_data_pred, 'v b l k n m c -> (v b c) (n m) l (k)')
+                test_data_prev = noise(test_data_prev, snr)
+                test_data_pred = noise(test_data_pred, snr)
+                std = np.sqrt(np.std(np.abs(test_data_prev) ** 2))
+                test_data_prev = test_data_prev / std
+                test_data_pred = test_data_pred / std
+                lens, _, _, _ = test_data_prev.shape
+                if model_test_enable[i] in ['clip', 'gpt', 'transformer', 'rnn', 'lstm', 'gru', 'cnn', 'np']:
+                    if model_test_enable[i] != 'np':
+                        model.eval()
+                    prev_data = LoadBatch_ofdm_2(test_data_prev)
+                    pred_data = LoadBatch_ofdm_2(test_data_pred)
+                    bs = 64
+                    cycle_times = lens // bs
+                    pth = 'code_testing/csi_output/{}'.format(date_)
+                    try:
+                        os.makedirs(pth)
+                    except:
+                        pass
+                    filename = pth + '/{}_{}dB.mat'.format((speed+1)*10, snr)
+                    ground_truth = []
+                    model_outputs = []
+                    with torch.no_grad():
+                        for cyt in range(cycle_times):
+                            prev = prev_data[cyt * bs:(cyt + 1) * bs, :, :].to(device)
+                            pred = pred_data[cyt * bs:(cyt + 1) * bs, :, :].to(device)
+                            prev = rearrange(prev, 'b m l k -> (b m) l k')
+                            pred = rearrange(pred, 'b m l k -> (b m) l k')
+                            if model_test_enable[i] == 'gpt':
+                                out = model(prev, None, None, None)
+                            elif model_test_enable[i] == 'transformer':
+                                encoder_input = prev
+                                dec_inp = torch.zeros_like(encoder_input[:, -pred_len:, :]).to(device)
+                                decoder_input = torch.cat([encoder_input[:, prev_len - label_len:prev_len, :], dec_inp],
+                                                        dim=1)
+                                out = model(encoder_input, decoder_input)
+                            elif model_test_enable[i] in ['lstm', 'rnn', 'gru']:
+                                out = model(prev, pred_len, device)
+                            elif model_test_enable[i] == 'cnn':
+                                out = model(prev)
+                            elif model_test_enable[i] == 'np':
+                                out = prev[:, [-1], :].repeat([1, pred_len, 1])
+                            elif model_test_enable[i] == 'clip':
+                                out = model(prev, None, None, None)
+                            loss = criterion(out, pred)
+                            test_loss_stack.append(loss.item())
+                            ground_truth.append(pred.cpu().detach().numpy())
+                            model_outputs.append(out.cpu().detach().numpy())
+                    savemat(filename, {'ground_truth':np.array(ground_truth),'model_output':np.array(model_outputs)})
+                    print("speed", (speed + 1) * 10, ":  NMSE:", np.nanmean(np.array(test_loss_stack)))
+                    NMSE[i].append(np.nanmean(np.array(test_loss_stack)))
 
-        fout_nmse = open(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()) + "_data_nmse_tdd_full.csv", "w")
+        fout_nmse = open(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()) + "_{}_data_nmse_fdd_full.csv".format(date_), "w")
         for row in NMSE:
             row = list(map(str, row))
             fout_nmse.write(','.join(row))
