1a59f304fae249f91cf0aaab22df7d92424f7311
wu <wus4nmu@gmail.com>
2025-03-27 18:54:45 +0800

    仿照timellm 输入前使用prompt和patch reprogramming 目前只用了CSI没有CIR

--- models/GPT4CP.py
+++ models/GPT4CP.py
@@ -1,15 +1,18 @@
 import os
 import numpy as np
 import torch
-import math
+from math import sqrt
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
-from transformers import GPT2ForSequenceClassification
+from transformers import CLIPTokenizer
 from transformers.models.gpt2.modeling_gpt2 import GPT2Model
 from transformers import CLIPModel, CLIPVisionModel, CLIPTextModel
 from einops import rearrange
 from Embed import DataEmbedding, VisionEmbedding
+
+from layers.Embed import PatchEmbedding
+from layers.StandardNorm import Normalize
 
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
@@ -54,8 +57,8 @@ class Res_block(nn.Module):
 class Model(nn.Module):
     model_list = ["gpt2", "clip", "clip_vision", "clip_text"]
 
-    # def __init__(self, gpt_type=model_list[3], d_ff=512, d_model=512, gpt_layers=6,  # done clip text
-    def __init__(self, gpt_type=model_list[2], d_ff=768, d_model=768, gpt_layers=6,  # done clip vision
+    def __init__(self, gpt_type=model_list[3], d_ff=512, d_model=512, gpt_layers=6,  # done clip text
+    # def __init__(self, gpt_type=model_list[2], d_ff=768, d_model=768, gpt_layers=6,  # done clip vision
     # def __init__(self, gpt_type=model_list[0], d_ff=768, d_model=768, gpt_layers=6,  # done gpt2
                  pred_len=4, prev_len=16, mlp=0, res_layers=4,
                  K=48, UQh=4, UQv=1, BQh=2, BQv=1,
@@ -70,6 +73,9 @@ class Model(nn.Module):
         self.stride = stride
         self.d_ff = d_ff
         self.d_model = d_model
+        self.n_heads = 8
+        self.enc_in_ = 7
+        self.seq_len = 96
 
         self.K = K
         self.UQh = UQh
@@ -84,6 +90,7 @@ class Model(nn.Module):
 
         self.enc_embedding1 = DataEmbedding(2 * self.enc_in, self.d_model, embed, freq, dropout)
         self.enc_embedding2 = VisionEmbedding(image_size=[self.prev_len, 2 * self.enc_in], patch_size=self.patch_size)
+
 
         if gpt_type == 'gpt2-medium':
             self.gpt2 = GPT2Model.from_pretrained('gpt2-medium', output_attentions=True, output_hidden_states=True)
@@ -103,8 +110,10 @@ class Model(nn.Module):
             self.gpt2 = CLIPModel.from_pretrained("./models/openai-clip-vit-base-patch32")
         elif gpt_type == 'clip_vision':
             self.gpt2 = CLIPVisionModel.from_pretrained("./models/openai-clip-vit-base-patch32")
+            self.tokenizer = CLIPTokenizer.from_pretrained("./models/openai-clip-vit-base-patch32")
         elif gpt_type == 'clip_text':
             self.gpt2 = CLIPTextModel.from_pretrained("./models/openai-clip-vit-base-patch32")
+            self.tokenizer = CLIPTokenizer.from_pretrained("./models/openai-clip-vit-base-patch32")
 
         else:
             self.gpt2 = GPT2Model.from_pretrained('./models/gpt2', output_attentions=True, output_hidden_states=True)
@@ -179,25 +188,39 @@ class Model(nn.Module):
         #     device = torch.device('cuda:{}'.format(gpu_id))
         #     self.gpt2.to(device=device)
 
-        self.patch_layer = nn.Linear(self.patch_size, self.patch_size)
-        self.patch_layer_fre = nn.Linear(self.patch_size, self.patch_size)
-        self.predict_linear_pre = nn.Linear(self.prev_len, self.prev_len)
-        self.vision_features = 1 + int(2 * self.enc_in * self.prev_len // (self.patch_size ** 2))
+        self.description = 'Channel state information (CSI) plays a fundamental role in facilitating m-MIMO related design'
+
+        self.patch_embedding = PatchEmbedding(self.d_model, self.patch_size, self.stride, dropout)
+
+        self.word_embeddings = self.gpt2.get_input_embeddings().weight
+        self.vocab_size = self.word_embeddings.shape[0]
+        self.num_tokens = 1000
+        self.mapping_layer = nn.Linear(self.vocab_size, self.num_tokens)
+        self.reprogramming_layer = ReprogrammingLayer(self.d_model, self.n_heads, self.d_ff, 512)#self.d_llm)
+        self.patch_nums = int((self.seq_len - self.patch_size) / self.stride + 2)
+        self.head_nf = self.d_ff * self.patch_nums
+        # self.output_projection = FlattenHead(self.enc_in_, self.head_nf, self.pred_len, head_dropout=dropout)
+        self.output_projection = FlattenHead(self.enc_in_, self.head_nf, 96, head_dropout=dropout)
+        self.normalize_layers = Normalize(self.enc_in_, affine=False)
+
+        # self.patch_layer = nn.Linear(self.patch_size, self.patch_size)
+        # self.patch_layer_fre = nn.Linear(self.patch_size, self.patch_size)
+        # self.predict_linear_pre = nn.Linear(self.prev_len, self.prev_len)
+        # self.vision_features = 1 + int(2 * self.enc_in * self.prev_len // (self.patch_size ** 2))
         '''
         self.vision_features是用kernel_size=patch_size,stride=patch_size的conv2d层
         处理尺寸为(2 * self.enc_in) * self.prev_len的x_enc_delay后
         结果的维数.
         例如：enc_in = 48, prev_len = 16, patch_size=4,
         则self.vision_features = 2*48/4 * 16/4 + 1= 97
-        '''
         self.predict_linear_vision_pre = nn.Linear(self.vision_features, self.vision_features)
 
         self.down_layer_vision_dim = nn.Linear(768, 512)
         self.down_layer_vision_time = nn.Linear(self.vision_features, self.prev_len)
         '''
+        '''
         clip的vision输出为[8, 97, 768]，需要变换到[8, 16, 512]，和text输出相同
         先下降dim再下降time
-        '''
         self.out_layer_dim = nn.Linear(d_ff, self.c_out * 2)
         self.output_layer_time = nn.Sequential(
             nn.Linear(self.prev_len, self.pred_len)
@@ -210,12 +233,67 @@ class Model(nn.Module):
             self.RB_f.append(Res_block(res_dim))
         self.RB_e.append(nn.Conv2d(res_dim, 2, 3, 1, 1))
         self.RB_f.append(nn.Conv2d(res_dim, 2, 3, 1, 1))
+        '''
 
     def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
+
+        x_enc = x_enc.permute(0, 2, 1)
+        # timellm里的顺序是batch_size\Time Steps\number of features，这里先调换过来
+
+        x_enc = self.normalize_layers(x_enc, 'norm')
+        '''
         mean = torch.mean(x_enc)
         std = torch.std(x_enc)
         x_enc = (x_enc - mean) / std
+        '''
+
+
         B, L, enc_in = x_enc.shape  # [B, L, D]  x_enc:torch.Size([1024, 16, 96])
+        # B T N -> B * N , T , 1
+        x_enc = x_enc.permute(0, 2, 1).contiguous().reshape(B * enc_in, L, 1)
+
+        min_values = torch.min(x_enc, dim=1)[0]
+        max_values = torch.max(x_enc, dim=1)[0]
+        medians = torch.median(x_enc, dim=1).values
+        lags = self.calcute_lags(x_enc)
+        trends = x_enc.diff(dim=1).sum(dim=1)
+
+        prompt = []
+        for b in range(x_enc.shape[0]):
+            min_values_str = str(min_values[b].tolist()[0])
+            max_values_str = str(max_values[b].tolist()[0])
+            median_values_str = str(medians[b].tolist()[0])
+            # lags_values_str = str(lags[b].tolist())
+            prompt_ = (
+                f"<|start_prompt|>Dataset description: {self.description}"
+                f"Task description: forecast the next {str(self.pred_len)} steps given the previous {str(self.prev_len)} steps information; "
+                "Input statistics: "
+                f"min value {min_values_str}, "
+                f"max value {max_values_str}, "
+                f"median value {median_values_str}, "
+                f"the trend of input is {'upward' if trends[b] > 0 else 'downward'}. "
+                # f"top 5 lags are : {lags_values_str}<|<end_prompt>|>"
+            )
+
+            prompt.append(prompt_)
+
+        x_enc = x_enc.reshape(B, enc_in, L).permute(0, 2, 1).contiguous()
+
+        prompt = self.tokenizer(prompt, return_tensors="pt", padding=True, truncation=True, max_length=2048).input_ids
+        prompt_embeddings = self.gpt2.get_input_embeddings()(prompt.to(x_enc.device))  # (batch, prompt_token, dim)
+
+        source_embeddings = self.mapping_layer(self.word_embeddings.permute(1, 0)).permute(1, 0)
+
+        x_enc = x_enc.permute(0, 2, 1).contiguous()
+        # x_enc = x_enc.to(torch.bfloat16)  # 临时加的
+        enc_out, n_vars = self.patch_embedding(x_enc)  # todo bfloat16?
+        # enc_out, n_vars = self.patch_embedding(x_enc.to(torch.bfloat16))
+        enc_out = self.reprogramming_layer(enc_out, source_embeddings, source_embeddings)
+        # llama_enc_out = torch.cat([prompt_embeddings, enc_out], dim=1)
+        # dec_out = self.llm_model(inputs_embeds=llama_enc_out).last_hidden_state
+        clip_enc_out = torch.cat([prompt_embeddings, enc_out], dim=1)
+
+        '''
         # process in delay domain
         x_enc_r = rearrange(x_enc, 'b l (k o) -> b l k o', o=2)  # torch.Size([1024, 16, 48, 2])
         x_enc_complex = torch.complex(x_enc_r[:, :, :, 0], x_enc_r[:, :, :, 1])  # torch.Size([1024, 16, 48])
@@ -249,11 +327,13 @@ class Model(nn.Module):
 
         enc_out = self.predict_linear_pre(enc_out.permute(0, 2, 1)).permute(0, 2, 1)
         # enc_out = torch.nn.functional.pad(enc_out, (0, self.gpt_dim - enc_out.shape[-1]))
+        '''
 
         # dec_out = self.gpt2(input_ids=x_enc_fre, pixel_values=x_enc_delay, return_loss=True)
         # dec_out = self.gpt2(pixel_values=enc_out)  # done clip
+        dec_out = self.gpt2(input_ids=clip_enc_out)  # done clip text
         # dec_out = self.gpt2(input_ids=enc_out)  # done clip text
-        dec_out = self.gpt2(pixel_values=enc_out)  # done clip vision
+        # dec_out = self.gpt2(pixel_values=enc_out)  # done clip vision
         # dec_out = self.gpt2(inputs_embeds=enc_out).last_hidden_state  # done gpt2 [B , L, 768]
         # clip_loss = dec_out.loss
 
@@ -261,19 +341,99 @@ class Model(nn.Module):
         # dec_out_text = dec_out.text_model_output.last_hidden_state  # [B, L, 512]
         # dec_out_vision = dec_out.vision_model_output.last_hidden_state  # [B, 1 + 16/patch_size * 96/patch_size, 768]
         dec_out = dec_out.last_hidden_state  # done clip & clipvision & clip text [B, L, 512]
-        dec_out = dec_out[:, :, :self.d_ff]
+        dec_out = dec_out[:, :, :self.d_ff]  # 128 可变 512
 
         # dec_out_vision = self.down_layer_vision_dim(dec_out_vision)
         # dec_out_vision = self.down_layer_vision_time(dec_out_vision.permute(0, 2, 1)).permute(0, 2, 1)
 
         # dec_out = dec_out_vision + dec_out_text
+        '''
         dec_out = self.out_layer_dim(dec_out)
         dec_out = self.output_layer_time(dec_out.permute(0, 2, 1)).permute(0, 2, 1)
 
         dec_out = dec_out * std + mean
-
+        '''
         # return clip_loss, dec_out[:, -self.pred_len:, :]  # [B, L, D]
-        return dec_out[:, -self.pred_len:, :]  # [B, L, D]
+
+        dec_out = torch.reshape(dec_out, (-1, n_vars, dec_out.shape[-2], dec_out.shape[-1]))
+        dec_out = dec_out.permute(0, 1, 3, 2).contiguous()  # 8,16，512 可变
+
+        # dec_out = dec_out[:, -self.pred_len:, :, -self.patch_nums:]
+        dec_out = dec_out[:, :, :, -self.patch_nums:]
+        dec_out = self.output_projection(dec_out)
+        dec_out = dec_out.permute(0, 2, 1).contiguous()
+
+        dec_out = self.normalize_layers(dec_out, 'denorm')
+        dec_out = dec_out.permute(0, 2, 1).contiguous()
+
+        dec_out = dec_out[:, :self.pred_len, :]
+        return dec_out
+
+        # return dec_out[:, -self.pred_len:, :]  # [B, L, D]
+    def calcute_lags(self, x_enc):
+        q_fft = torch.fft.rfft(x_enc.permute(0, 2, 1).contiguous(), dim=-1)
+        k_fft = torch.fft.rfft(x_enc.permute(0, 2, 1).contiguous(), dim=-1)
+        res = q_fft * torch.conj(k_fft)
+        corr = torch.fft.irfft(res, dim=-1)
+        mean_value = torch.mean(corr, dim=1)
+        _, lags = torch.topk(mean_value, 5, dim=-1)
+        return lags
+
+class ReprogrammingLayer(nn.Module):
+    def __init__(self, d_model, n_heads, d_keys=None, d_llm=None, attention_dropout=0.1):
+        super(ReprogrammingLayer, self).__init__()
+
+        d_keys = d_keys or (d_model // n_heads)
+
+        self.query_projection = nn.Linear(d_model, d_keys * n_heads)
+        self.key_projection = nn.Linear(d_llm, d_keys * n_heads)
+        self.value_projection = nn.Linear(d_llm, d_keys * n_heads)
+        self.out_projection = nn.Linear(d_keys * n_heads, d_llm)
+        self.n_heads = n_heads
+        self.dropout = nn.Dropout(attention_dropout)
+
+    def forward(self, target_embedding, source_embedding, value_embedding):
+        B, L, _ = target_embedding.shape
+        S, _ = source_embedding.shape
+        H = self.n_heads
+
+        target_embedding = self.query_projection(target_embedding).view(B, L, H, -1)
+        source_embedding = self.key_projection(source_embedding).view(S, H, -1)
+        value_embedding = self.value_projection(value_embedding).view(S, H, -1)
+
+        out = self.reprogramming(target_embedding, source_embedding, value_embedding)
+
+        out = out.reshape(B, L, -1)
+
+        return self.out_projection(out)
+
+    def reprogramming(self, target_embedding, source_embedding, value_embedding):
+        B, L, H, E = target_embedding.shape
+
+        scale = 1. / sqrt(E)
+
+        scores = torch.einsum("blhe,she->bhls", target_embedding, source_embedding)
+
+        A = self.dropout(torch.softmax(scale * scores, dim=-1))
+        reprogramming_embedding = torch.einsum("bhls,she->blhe", A, value_embedding)
+
+        return reprogramming_embedding
+
+
+
+class FlattenHead(nn.Module):
+    def __init__(self, n_vars, nf, target_window, head_dropout):
+        super().__init__()
+        self.n_vars = n_vars
+        self.flatten = nn.Flatten(start_dim=-2)
+        self.linear = nn.Linear(nf, target_window)
+        self.dropout = nn.Dropout(head_dropout)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.linear(x)
+        x = self.dropout(x)
+        return x
 
 if __name__ == '__main__':
     import torch
