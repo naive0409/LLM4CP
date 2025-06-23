888a08e3494372e10e1c4bce24a3459edebde138
Wu Sanmu <wus4nmu@gmail.com>
2025-06-23 10:12:40 +0800

    test: 修改控制台输出格式，用\t对齐

--- test_tdd_full.py
+++ test_tdd_full.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     prev_path = "./Testing Dataset/H_U_his_test.mat"      # path of dataset [H_U_his_test]
     pred_path = "./Testing Dataset/H_U_pre_test.mat"      # path of dataset [H_U_pre_test]
     pred_path_fdd = "./Testing Dataset/H_D_pre_test.mat"  # path of dataset [H_D_pre_test]
-    date_ = '20250506_16_11'
+    date_ = '20250619_22_43'
     model_path = {
         'clip': 'Weights/full_shot_tdd/{}/clip.pth'.format(date_),
         'gpt': './Weights/full_shot_tdd/U2U_LLM4CP.pth',
@@ -118,7 +118,7 @@ if __name__ == "__main__":
                             ground_truth.append(pred.cpu().detach().numpy())
                             model_outputs.append(out.cpu().detach().numpy())
                     savemat(filename, {'ground_truth':np.array(ground_truth),'model_output':np.array(model_outputs)})
-                    print("speed:", (speed + 1) * 10, "snr:", snr, ": NMSE:", np.nanmean(np.array(test_loss_stack)))
+                    print("speed:", (speed + 1) * 10, "snr:", snr, "\t: NMSE:", np.nanmean(np.array(test_loss_stack)))
                     NMSE[i].append(np.nanmean(np.array(test_loss_stack)))
                 elif model_test_enable[i] in ['pad', 'pvec']:
                     cycle_times = lens
